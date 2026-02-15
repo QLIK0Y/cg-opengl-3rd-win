08c68976deddcc0456010dc378e7328faa65d3fb
Arthur Lee <boyal2008@gmail.com>
2026-02-15 22:36:36 +0800

    change color in fragment shader

--- point.cpp
+++ point.cpp
@@ -21,7 +21,11 @@ void createShaderProgram() {
 		out vec4 color;
 		void main(void)
 		{
-			color = vec4(1.0, 0.0, 0.0, 1.0);
+			if (gl_FragCoord.x < 400.0) {
+				color = vec4(0.0, 1.0, 0.0, 1.0);
+			} else {
+				color = vec4(0.0, 0.0, 1.0, 1.0);
+			}
 		}
 	)";
 
